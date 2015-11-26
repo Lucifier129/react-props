9c96503a699786547d7f48db4f58d42507847a32
古映杰 <yj_gu@Ctrip.com>
2015-11-26 18:11:15 +0800

    update webpack.config

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -10,6 +10,14 @@ module.exports = {
     library: 'ReactProps',
     libraryTarget: 'umd'
   },
+  externals: {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  },
   resolve: {
     extensions: ['', '.js']
   }
